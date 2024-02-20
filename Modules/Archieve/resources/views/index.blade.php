6dfc8994b58e0cc75ac85a01d26bd09c996b8852
Muhammad Syaoki Faradisa <syaokifaradisa09@gmail.com>
2024-02-20 10:29:59 +0800

    feat(archieve): create dashboard

--- Modules/Archieve/resources/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('archieve::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('archieve.name') !!}</p>
-@endsection
--- Modules/Archieve/routes/web.php
+++ Modules/Archieve/routes/web.php
@@ -1,19 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Archieve\App\Http\Controllers\ArchieveController;
+use Modules\Archieve\App\Http\Controllers\DashboardController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::group([], function () {
-    Route::resource('archieve', ArchieveController::class)->names('archieve');
-});
+Route::prefix('archieve')
+    ->name('archieve.')
+    ->group(function(){
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    }
+);
